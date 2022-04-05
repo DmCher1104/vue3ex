59bc1aa035b146b38bfc90c47c0ae6adabccf879
dmitry.chernetsov <dmitry.chernetsov@ntlab.com>
2022-04-05 23:54:02 +0300

    init ex-s with spa1

---
+++ src/components/PostForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <form @submit.prevent>
+    <h3>Создание поста</h3>
+    <my-input
+        v-model="post.title"
+        @input="post.title = $event.target.value"
+        type="text"
+        placeholder="Название"
+    />
+    <my-input
+        v-model="post.body"
+        @input="post.body = $event.target.value"
+        type="text"
+        placeholder="Описание"
+    />
+    <my-btn
+        style="align-self: flex-end; margin-top: 15px"
+        @click="createPost"
+    >
+      создать
+    </my-btn>
+  </form>
+</template>
+
+<script>
+import MyInput from "@/components/UI/MyInput";
+
+export default {
+  components: {MyInput},
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      }
+    }
+  },
+  methods: {
+    createPost(event) {
+      this.post.id = Date.now();
+      this.$emit('create', this.post);
+      this.post = {
+        title: '',
+        body: '',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
